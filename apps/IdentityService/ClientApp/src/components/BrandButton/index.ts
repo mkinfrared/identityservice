5fb5a0da2e128dedd5ece8d8fdc85d2ffc4baa8f
Mikhail Korotkov <mkinfrared@gmail.com>
2022-05-15 12:38:47 +0300

    feat: update dependencies
    * update to react 18
    * update to typescript 4.6
    * update to stylelint 14
    * update to use pnpm 7
    * update .net to use version 6.0 and higher
    
    Closes IS-73

--- apps/IdentityService/ClientApp/src/components/BrandButton/index.ts
+++ apps/IdentityService/ClientApp/src/components/BrandButton/index.ts
@@ -1,5 +1,3 @@
-// TODO Remove disable when BrandButton is used in the app
-// eslint-disable-next-line import/no-unused-modules
 export { default } from "./BrandButton";
 
 export * from "./BrandButton.type";
