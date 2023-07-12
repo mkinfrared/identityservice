5e6bb569769fcd3b48354ebcd1ef1eacf757d564
Mikhail Korotkov <mkinfrared@gmail.com>
2023-07-12 20:20:08 +0400

    perf: add esbuild loader to build apps
    * Update JavaScript dependencies for build performance and compatibility
    
    * These changes are expected to impact the build performance positively and lead to shorter build times.
    
    Closes #92

--- apps/IdentityService/ClientApp/webpack.config.js
+++ apps/IdentityService/ClientApp/webpack.config.js
@@ -3,6 +3,7 @@ const path = require("path");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const Dotenv = require("dotenv-webpack");
+const { EsbuildPlugin } = require("esbuild-loader");
 const ForkTsCheckerWebpackPlugin = require("fork-ts-checker-webpack-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
@@ -131,7 +132,11 @@ const rules = [
   },
   {
     test: /\.(js|mjs|jsx|ts|tsx)$/,
-    use: ["babel-loader"],
+    loader: "esbuild-loader",
+    options: {
+      // JavaScript version to compile to
+      target: "es2020",
+    },
   },
   {
     test: /\.svg$/,
@@ -219,42 +224,8 @@ const config = {
     runtimeChunk: true,
     minimize: true,
     minimizer: [
-      new TerserPlugin({
-        terserOptions: {
-          parse: {
-            // We want terser to parse ecma 8 code. However, we don't want it
-            // to apply any minification steps that turns valid ecma 5 code
-            // into invalid ecma 5 code. This is why the 'compress' and 'output'
-            // sections only apply transformations that are ecma 5 safe
-            // https://github.com/facebook/create-react-app/pull/4234
-            ecma: 8,
-          },
-          compress: {
-            ecma: 5,
-            warnings: false,
-            // Disabled because of an issue with Uglify breaking seemingly valid code:
-            // https://github.com/facebook/create-react-app/issues/2376
-            // Pending further investigation:
-            // https://github.com/mishoo/UglifyJS2/issues/2011
-            comparisons: false,
-            // Disabled because of an issue with Terser breaking valid code:
-            // https://github.com/facebook/create-react-app/issues/5250
-            // Pending further investigation:
-            // https://github.com/terser-js/terser/issues/120
-            inline: 2,
-          },
-          mangle: {
-            safari10: true,
-          },
-          // Added for profiling in devtools
-          output: {
-            ecma: 5,
-            comments: false,
-            // Turned on because emoji and regex is not minified properly using default
-            // https://github.com/facebook/create-react-app/issues/2488
-            ascii_only: true,
-          },
-        },
+      new EsbuildPlugin({
+        target: "es2020", // Syntax to compile to (see options below for possible values)
       }),
       new CssMinimizerPlugin(),
     ],
